67d65568ffd2d478791857290ccc9826095d5be6
강석현 <tjrgus0713@gmail.com>
2021-11-29 13:31:49 +0900

    [추가] 랜딩페이지 퍼블리싱

--- src/containers/landingPage/index.tsx
+++ src/containers/landingPage/index.tsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import * as S from './styles'
-
+import Logo from '../../assets/images/logo.svg'
+import BackGround from '../../assets/images/landing/background.svg'
 function Landing() {
     return (
         <S.LandingWrapper>
-
+            <S.LandingTop>
+                <div>
+                    <i><img src={Logo} alt="" /></i>
+                    <p>정윤이의 다이어리</p>
+                </div>
+                <S.Line />
+            </S.LandingTop>
+            <S.LaindingMain img={BackGround}>
+                <S.LoginBox>
+                    <p>더 많은 서비스를 이용하시려면 로그인이 필요합니다</p>
+                    <S.LoginBtn>로그인 / 회원가입 바로가기</S.LoginBtn>
+                </S.LoginBox>
+            </S.LaindingMain>
         </S.LandingWrapper>
     )
 }
